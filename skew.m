84b63e2cc7dcc815a0974f32d29a5e00b59b37b9
dollishdiva <hanna02pro@gmail.com>
2024-10-03 20:56:50 -0700

    skew.m
    updated skew to allow for skew symmetric matrix for screws

--- skew.m
+++ skew.m
@@ -1,11 +1,16 @@
-function skew = skew(w)
-%creates skew symmetric matrix for column or row vector (size 3)
+function skew = skew(w,v)
+%creates skew symmetric matrix for column or row vector (size 3), or screw
+%given w and lin vel v
 
-if size(w,2) == 1 || size(w,1) == 1 
-    skew = [0 -w(3) w(2); w(3) 0 -w(1); -w(2) w(1) 0];
-else
-    disp("skew matrix not possible with this vector");
+switch nargin
+    case 1
+    if size(w,2) == 1 || size(w,1) == 1 
+        skew = [0 -w(3) w(2); w(3) 0 -w(1); -w(2) w(1) 0];
+    else
+        disp("skew matrix not possible with this vector");
+    end
+    otherwise
+        skew = [0 -w(3) w(2) v(1); w(3) 0 -w(1) v(2); -w(2) w(1) 0 v(3); 0 0 0 0];
+
 end
-
-
 end
